d6ff885c52ce624a3c6c7bf8ac40d21d1201ab65
Shashank N <shashank.n@vvce.ac.in>
2021-03-20 19:46:07 +0530

    Create Program-1.cpp

---
+++ Program-1.cpp
@@ -0,0 +1,23 @@
+//Find the missing in the given C++ program to add two integer numbers.
+
+//Instructions
+
+//In this program, the user enters two integer number of his choice with the help of standard input.
+//The addition operation is performed.
+//The Output for this program is the sum of two entered numbers that will be displayed on the standard output.
+
+//Expected input and output
+//Input: Enter two numbers: 2 3
+//Output: The sum of two numbers is 5
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,s;
+    std::cout << "Enter two integers: ";
+    std::cin >> x >> y;
+                    // Insert the missing statement to find sum of two numbers in stored in variable s
+    std::cout << ;  // Print the sum by inserting the correct variable
+    return 0;
+}
